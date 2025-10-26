0d05ad514a10a30b29dae4f4e9e2c969cc2eee14
jimmy <jimmycorreia3@gmail.com>
2025-10-26 12:29:42 -0400

    Changed theme, added clock, resized windows.

--- gui.py
+++ gui.py
@@ -2,10 +2,12 @@ from os import close
 
 import functions as fn
 import FreeSimpleGUI as sg
-
+import time
 from functions import write_to_file
 
 # Define needed elements
+sg.theme("Black")
+clock = sg.Text('', key='clock')
 label1 = sg.Text("Type in a Website")
 label2 = sg.Text("Enter desired number of upper case characters")
 label3 = sg.Text("Enter desired number of lower case characters")
@@ -13,35 +15,53 @@ label4 = sg.Text("Enter desired number of numerical characters")
 label5 = sg.Text("Enter desired number of special characters")
 website_edit_desc_label = sg.Text("Enter new name of the website")
 
-website_box = sg.InputText(tooltip = "Enter Website", key = "website")
-upper_case_box = sg.InputText(tooltip = "Enter desired number of upper case characters",
-                              key = "upper_case")
-lower_case_box = sg.InputText(tooltip= "Enter desired number of lower case characters",
-                              key = "lower_case")
-numbers_box = sg.InputText(tooltip= "Enter desired number of numerical characters",
-                              key = "number_case")
-special_box = sg.InputText(tooltip= "Enter desired number of special characters",
-                              key = "special_case")
+row1 = [sg.Text("Type in a Website", size=(35,1), justification='left'),
+        sg.Input(key="website", expand_x=True)]
 
-edit_website_box = sg.InputText(tooltip="Enter the new name of the website",
-                                key = "new_website")
+row2 = [sg.Text("Enter desired number of upper case characters", size=(35,1), justification='left'),
+        sg.Input(key="upper_case", size=(2,1))]
+
+row3 = [sg.Text("Enter desired number of lower case characters", size=(35,1), justification='left'),
+        sg.Input(key="lower_case", size=(2,1))]
+
+row4 = [sg.Text("Enter desired number of numerical characters", size=(35,1), justification='left'),
+        sg.Input(key="number_case", size=(2,1))]
+
+row5 = [sg.Text("Enter desired number of special characters", size=(35,1), justification='left'),
+        sg.Input(key="special_case", size=(2,1))]
+
+
 submit_button = sg.Button("Generate")
 display_button = sg.Button("Display")
 
-window = sg.Window('My Password Manager', layout=[[[label1],[website_box]],
-                                                  [[label2],[upper_case_box]],
-                                                  [[label3],[lower_case_box]],
-                                                  [[label4],[numbers_box]],
-                                                  [[label5],[special_box]],
-                                                  [[submit_button,display_button]]],
-                   font=('Times New Roman', 15))
+window = sg.Window(
+    'My Password Manager',
+    layout=[
+        [clock],
+        [row1],
+        [row2],
+        [row3],
+        [row4],
+        [row5],
+        [submit_button, display_button],
+    ],
+    font=('Times New Roman', 15), element_padding=(10,5))
+
+
+
+
 
 window2 = None
 window2_open = False
 while True:
     # polls window to receive updates
     event, values = window.read(timeout=100)
+    if event == sg.WIN_CLOSED:
+        break
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
+
         case 'Generate':
             try:
                 website = values['website']
@@ -56,22 +76,24 @@ while True:
                     new_content = fn.view_file()
                     window2['passwords'].update(new_content)
 
-                generate_popup = sg.popup("Password Generated", custom_text="Okay")
+                generate_popup = sg.popup("Password Generated",
+                                          custom_text="Okay")
             except ValueError:
                 sg.popup("Please type the desired number of characters into the relevant boxes",
                          custom_text = "Okay" )
-        case sg.WIN_CLOSED:
-            break
+
         case 'Display':
             passwords_file = fn.view_file()
             formatted_passwords_file = fn.format_password_list(passwords_file)
 
             secrets_revealed = sg.Listbox(values=fn.view_file(), key='passwords',
-                                          enable_events=True, size=[45, 10])
+                                          enable_events=True,
+                                          size=[45, 10])
             edit_button = sg.Button("Regenerate")
             delete_button = sg.Button("Delete")
 
-            window2 = sg.Window('Passwords Master List', layout=[[secrets_revealed], [[edit_button,delete_button]]],
+            window2 = sg.Window('Passwords Master List', layout=[[secrets_revealed],
+                                                                 [[edit_button,delete_button]]],
                                 font=('Times New Roman', 15))
             window2_open = True
             event, value = window2.read(timeout=0)
